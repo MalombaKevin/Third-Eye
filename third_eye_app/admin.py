56d4d86c285bed37b0593d19097560e8e087ffb9
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-22 02:56:35 +0300

    perf: admin dashboard - Add admin link to the nav bar

--- third_eye_app/admin.py
+++ third_eye_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from third_eye_app.models import Business, Profile, User_Posts
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(User_Posts)
+
--- third_eye_app/views.py
+++ third_eye_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+# @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
 
